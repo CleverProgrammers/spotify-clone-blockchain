d5550aea5e4e3b2a55ccb043b035ac41a54bde7c
David Rakosi <61084415+davidrakosi@users.noreply.github.com>
2022-05-20 13:35:01 -0700

    ✅ Update Songs

--- data/activities.js
+++ data/activities.js
@@ -1,22 +1,22 @@
 export const activities = [
   {
     index: 1,
-    title: 'Ambient Gold',
-    subTitle: 'Lo-Fi',
-    avatar:
-      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+    title: 'Enter Sandman',
+    subTitle: 'Metallica',
+    avatar: 'https://i.scdn.co/image/ab6761610000e5eb8101d13bdd630b0889acd2fd',
   },
   {
     index: 2,
-    title: 'Kill it anyway',
-    subTitle: 'SteamBeats originals',
+    title: "Drop it Like it's Hot",
+    subTitle: 'Snoop Dogg',
     avatar:
-      'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
+      'https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg',
   },
   {
     index: 3,
-    title: 'Gift of a Guest',
-    subTitle: 'SteamBeats originals',
-    avatar: 'https://i.ytimg.com/vi/Yaeu73hr_eM/maxresdefault.jpg',
+    title: 'goosebumps',
+    subTitle: 'Travis Scott',
+    avatar:
+      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRhR6m3xIp4gsabf58BUMxlt9KKOe2IDsBFJZIZ5BjkXpOjS_YY',
   },
 ]
--- data/songs.js
+++ data/songs.js
@@ -1,37 +1,24 @@
 export const songs = [
   {
-    index: 0,
-    title: 'Ambient Gold',
+    index: 1,
+    title: 'Path Less Traveled',
     artiste: 'Lo-Fi',
     plays: '20,382',
     songLength: '2:43',
     album: 'Songs & Beats',
     cover:
       'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
-    arweaveLink:
-      'https://tnrapvewequ7ofz7idqmz6xlqmfkplivoyfosxagcpg23rxe.arweave.net/m2IH1JYkKfcXP0DgzPrrgwqnrRV2CulcBhPN_rc_bkY',
+    link: 'https://res.cloudinary.com/dgjl7f1o0/video/upload/v1653077699/spotify-clone/Copy_of_1._Harris_Heller_-_Path_Less_Traveled_hlxpnf.mp3',
   },
   {
-    index: 1,
-    title: 'Kill it anyway',
+    index: 2,
+    title: 'Radiant Vibes',
     artiste: 'SteamBeats originals',
     plays: '1,324',
     songLength: '4:50',
     album: 'The Random House',
     cover:
       'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
-    arweaveLink:
-      'https://rnvytwtlgebo5e57kjl5ax3omeow423vmeul25dwmgueyhst.arweave.net/i2uJ2msxAu6Tv1JX0F9uYR1ua_-3VhKL10dmGoTB5TA',
-  },
-  {
-    index: 2,
-    title: 'Gift of a Guest',
-    artiste: 'SteamBeats originals',
-    plays: '45,422',
-    songLength: '1:33',
-    album: 'A Regular Album',
-    cover: 'https://i.ytimg.com/vi/Yaeu73hr_eM/maxresdefault.jpg',
-    arweaveLink:
-      'https://dr5vuvoh5dm7pdnv5i4qo5ezmfmqevs4ytrx5c3wskdby4dy.arweave.net/HHta_Vcfo2feNt-eo5_B3SZYVkCVlzE436LdpKGHHB4',
+    link: 'https://res.cloudinary.com/dgjl7f1o0/video/upload/v1653077728/spotify-clone/Copy_of_2._Harris_Heller_-_Radiant_Vibes_b62ofa.mp3',
   },
 ]
